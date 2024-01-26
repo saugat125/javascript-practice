ccae46f8e47461e7e40812dabb1a60bd477bc3ab
Saugat-Rajbhandari <107553702+Saugat-Rajbhandari@users.noreply.github.com>
2024-01-26 17:09:56 +0545

    color switcher

---
+++ color-switcher/script.js
@@ -0,0 +1,12 @@
+// let dropdown = document.getElementById('dropdown');
+
+// dropdown.addEventListener('change',(value)=>{
+//     let color = value.target.value;
+//     document.body.style.backgroundColor = color;
+// })
+
+let dropdown = document.getElementById('dropdown');
+
+function colorChange(value){
+    document.body.style.backgroundColor = value; 
+}
