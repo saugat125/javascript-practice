055c084017b946b003df367bec151a7b2ebd7680
Saugat-Rajbhandari <107553702+Saugat-Rajbhandari@users.noreply.github.com>
2024-01-27 12:43:42 +0545

    guesser

---
+++ number-guesser/script.js
@@ -0,0 +1,35 @@
+let submit = document.getElementById('submitbtn');
+let result = document.getElementById('result');
+
+let random_number = Math.floor(Math.random() * 100);
+console.log(random_number);
+
+submit.addEventListener('click', checkNumber);
+let count = 1;
+
+function checkNumber(){
+    let userInput = document.getElementById('number').value;
+    if(userInput>0){
+        if (userInput == random_number) {
+            result.innerHTML = 'Correct answer';
+        } else if (userInput < random_number) {
+            result.innerHTML = 'The number entered is lower';
+            
+        } else if (userInput > random_number) {
+            result.innerHTML = 'The number entered is higher';
+            
+        }
+        count = count + 1;
+        if(count>3){
+            result.innerHTML = "You only had 3 chances you lost"
+        }
+    }
+    else{
+        result.innerHTML = "Enter number from 1 to 100";
+    }
+}
+
+
+
+
+
