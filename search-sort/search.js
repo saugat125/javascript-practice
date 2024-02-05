2254591158571789639828c9025ee3b1b04cc6cf
Saugat-Rajbhandari <107553702+Saugat-Rajbhandari@users.noreply.github.com>
2024-02-05 11:56:08 +0545

    binary search

--- search-sort/search.js
+++ search-sort/search.js
@@ -11,3 +11,28 @@ let arraylist = [1,2,3,4,5];
 let targetElement = 3;
 console.log(linearSearch(arraylist,targetElement));
 
+function binarySearch(array,target){
+    let low = 0;
+    let high = array.length-1;
+
+    while(low<=high){
+        let mid = Math.floor((low+high)/2);
+
+        if(array[mid]===target){
+            return mid;
+        }
+        else if(array[mid]<target){
+            low = low + 1;
+        }
+        else{
+            high = high - 1;
+        }
+    }
+    return -1;
+}
+
+let binarylist = [1,2,3,4,5];
+let targetelement = 1;
+console.log(binarySearch(binarylist,targetelement));
+
+
