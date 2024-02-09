f1e363493b0e5b93477c58241e79d99369e0ec78
Saugat-Rajbhandari <107553702+Saugat-Rajbhandari@users.noreply.github.com>
2024-02-09 14:34:00 +0545

    object

---
+++ dsa-questions/object.js
@@ -0,0 +1,11 @@
+let person = {
+    name : 'John',
+    age : '20',
+    gender : 'male'
+};
+
+console.log(person.name);
+console.log(person['name']);
+
+person.country = 'Pakistan';
+console.log(person);
