4f8c9d60cdb303f8a6ff5819cc9147034e390e6f
Saugat-Rajbhandari <107553702+Saugat-Rajbhandari@users.noreply.github.com>
2024-02-28 15:02:45 +0545

    small change

---
+++ test/react.js
@@ -0,0 +1,14 @@
+function MyButton(){
+    return (
+        <button>This is a button</button>
+    );
+}
+
+function MyApp(){
+    return (
+        <>
+        <h1 className="head">Heading</h1>
+        <MyButton />
+        </>
+    );
+}
